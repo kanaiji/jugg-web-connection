0bff334a15eefe11e61555b30041b613b19db775
kanaiji <937334241@qq.com>
2018-09-11 09:10:07 +0800

    aaa

---
+++ src/main/java/com/jugg/web/connection/middleware/rmq/producter/MsgSendConfirmCallBack.java
@@ -0,0 +1,26 @@
+package com.jugg.web.connection.middleware.rmq.producter;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
+ 
+public class MsgSendConfirmCallBack implements RabbitTemplate.ConfirmCallback {
+	
+	
+	private Logger logger = LoggerFactory.getLogger(MsgSendReturnCallback.class);
+	
+	
+    public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+        if (ack) {
+        	logger.info("消息确认成功cause:"+cause);
+        } else {
+        	//处理丢失的消息
+        	logger.error("消息确认失败:"+correlationData.getId()+"#cause:"+cause);
+        }
+    }
+    
+    
+    
+}
