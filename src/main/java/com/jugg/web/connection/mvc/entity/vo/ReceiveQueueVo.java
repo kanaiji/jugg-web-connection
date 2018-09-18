32b1eb4da0b96f681d0ca9132997b7a515fbea13
kanaiji <937334241@qq.com>
2018-09-18 16:40:58 +0800

    补充

--- src/main/java/com/jugg/web/connection/mvc/entity/vo/ReceiveQueueVo.java
+++ src/main/java/com/jugg/web/connection/mvc/entity/vo/ReceiveQueueVo.java
@@ -34,9 +34,34 @@ public class ReceiveQueueVo {
 		this.fileId = fileId;
 	}
 
+	public String getLogId() {
+		return logId;
+	}
+
+	public void setLogId(String logId) {
+		this.logId = logId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getJob_id() {
+		return job_id;
+	}
+
+	public void setJob_id(String job_id) {
+		this.job_id = job_id;
+	}
+
 	@Override
 	public String toString() {
-		return "MsgVo [connectionId=" + connectionId + ", fileId=" + fileId + "]";
+		return "ReceiveQueueVo [connectionId=" + connectionId + ", fileId=" + fileId + ", logId=" + logId + ", type="
+				+ type + ", job_id=" + job_id + "]";
 	}
 	
 	
