7b3a02acaaee2e267e39dee7ea455fa75840a14f
kanaiji <937334241@qq.com>
2018-09-19 11:42:15 +0800

    idea commit code 测试。

--- src/main/java/com/jugg/web/connection/Application.java
+++ src/main/java/com/jugg/web/connection/Application.java
@@ -10,10 +10,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-//在部署到外部的Tomcat时，需要将classpath的引入文件去掉，因为在web.xml已经配置过一次了
 
-//解決，当spring创建dataSource bean因缺少相关的信息就会报错。 implements CommandLineRunner 
+//在部署到外部的Tomcat时，需要将classpath的引入文件去掉，因为在web.xml已经配置过一次了
+@SpringBootApplication
+//解決，当spring创建dataSource bean因缺少相关的信息就会报错。 implements CommandLineRunner
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableConfigurationProperties
 public class Application extends SpringBootServletInitializer{
@@ -21,7 +21,6 @@ public class Application extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 	// Java EE应用服务器配置，
 	// 如果要使用tomcat来加载jsp的话就必须继承SpringBootServletInitializer类并且重写其中configure方法
 	@Override
