d5420acca1018dcecf338f8ffa786bfa3fa78bf9
kanaiji <937334241@qq.com>
2018-11-14 13:01:20 +0800

    修改了 build

--- build.sh
+++ build.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-ROOT=$1
-mvn -U -am clean package -Dmaven.test.skip=true
+mvn clean package -Dmaven.test.skip=true
 
 IMAGE_NAME=9.42.41.226:5000/jugg-web-connection/jugg-web-connection:latest
 
 echo "${IMAGE_NAME}" > IMAGE_NAME
-
-#jenkins 根目录/root/.jenkins/workspace/jugg-web-connection
-cd ${ROOT}/jugg-web-connection
-docker build -t ${IMAGE_NAME} .
-cd -
-
-docker push ${IMAGE_NAME}
-
 echo "jugg-web-connection" > MODULE
+
+docker build -t ${IMAGE_NAME} .
+docker push ${IMAGE_NAME}
