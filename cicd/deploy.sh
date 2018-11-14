cdc14d1bd7b1e5ae613415892fde56fd343e4360
kanaiji <937334241@qq.com>
2018-11-14 16:45:13 +0800

    处理了 deploy.sh 中的  if else if else .

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -9,11 +9,11 @@ echo "start deploy jugg connection...${DEPLOYMENTS}  ${MODULE}=${IMAGE}"
 #操作
 OPERATE=$2
 echo "OPERATE = ${OPERATE}"
-if [[ OPERATE == "apply" ]];then
+if [ ${OPERATE} == 'apply' ];then
     echo "kubectl apply -f /root/.jenkins/workspace/jugg-web-connection/cicd/jugg-web-connection-deployment.yaml"
          #第一次部署, 找 yaml
    kubectl apply -f /root/.jenkins/workspace/jugg-web-connection/cicd/jugg-web-connection-deployment.yaml
-elif [[ OPERATE == "update" ]];then
+elif [ ${OPERATE} == 'update' ];then
     echo "kubectl set image deployments/${DEPLOYMENTS}  ${MODULE}=${IMAGE}"
           # 直接更新版本 kubectl set image deployments/${DEPLOYMENTS}  ${MODULE}=${IMAGE}
     kubectl set image deployments/${DEPLOYMENTS}  ${MODULE}=${IMAGE}
